f3fbe3cdd6a219df8d6a273f594a944d8c2a042e
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-26 13:10:52 -0300

    feat/creation Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <section className="wrapper-loading">
+      <div className="box-filter-preview" />
+      <div className="box-recipe-preview" />
+    </section>
+  );
+}
+
+export default Loading;
